aa24972983e385fcc2f53c8599674fcd756ca8af
Matthieu Mohimbouabeka <mohimbouabeka@gmail.com>
2021-09-18 11:55:08 +0200

    mise en place des test unitaires

---
+++ spec/tictactripSpec.js
@@ -0,0 +1,14 @@
+const utils = require("../index.js");
+describe("String Utils", function () {
+    it("le texte devrait être justifié", function () {
+        text = "azerty"
+        justifiedText = utils.transformTextToTextjustify(text)
+        expect(justifiedText).toEqual(text);
+    });
+    it("le texte d'une longueur supérieur à 80 devrait être justifié", function () {
+        text = "im only happy when it's rains im only happy when it's rains im only happy when it's rains im only happy when it's rains im only happy when it's rains im only happy when it's rains im only happy when it's rains"
+        justifiedText = utils.transformTextToTextjustify(text)
+        resultText = "im only happy when it's rains im only happy when it's rains im only happy when\nit's rains im only happy when it's rains im only happy when it's rains im only\nhappy when it's rains im only happy when it's rains"
+        expect(justifiedText).toEqual(resultText);
+    });
+ });
